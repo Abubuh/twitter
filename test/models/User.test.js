9b7ee1f4f4016de0fd0e79fcdaa88eebc9753bf1
cristian <Cristian_caht@hotmail.com>
2022-05-04 17:53:03 -0500

    5. Se añadio segunda prueba sobre Fechas de usuarios

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,15 @@
 const User = require("../../app/models/User")
 
 describe("Prueba de jest", () =>{
-    test("Creacion de User", () =>{
+    test("Requerimiento 1: Creacion de User", () =>{
     const user = new User(1, 'Cristian hdz', 'Cris', 'Bio', 'dateCreated', 'lastUpdated')
     expect(user.id).toBe(1)
     expect(user.username).toBe('Cristian hdz')
-})
+    })
+    test("Requerimiento 2: Fechas en atributos de User", () =>{
+        const user = new User(1, 'Cristian hdz', 'Cris', 'Bio')
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+
+    })
 })
